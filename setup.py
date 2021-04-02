91bf8349c72a9e5798af66e701fc9cca555d1e91
Riei Joaquim <rjmr@cin.ufpe.br>
2021-04-02 12:59:44 -0300

    Fixing organization

---
+++ setup.py
@@ -0,0 +1,9 @@
+'''
+#       Install Project Requirements 
+'''
+from setuptools import setup
+
+setup(name='VotingProtocol',
+    version='1.0',
+    install_requires=[]
+)
---
+++ src/Entities/__init__.py
@@ -0,0 +1,12 @@
+from Entities.ClientData import ClientData
+from Entities.ClientRequests import ClientRequests, ClientRequestCreate
+from Entities.EvaluationData import EvaluationData
+from Entities.FinishConnection import FinishConnection
+from Entities.HelloClient import HelloClient
+from Entities.HelloServers import HelloServers
+from Entities.RequestResponse import RequestResponse
+from Entities.SessionDetails import SessionDetails, SessionDescription
+from Entities.Vote import Vote
+from Entities.VoteResponse import VoteResponse
+
+__all__ = ['ClientData', 'EvaluationData', 'FinishConnection', 'HelloClient', 'HelloServers', 'HelloServers', 'RequestResponse', 'SessionDetails', 'Vote', 'VoteResponse']
