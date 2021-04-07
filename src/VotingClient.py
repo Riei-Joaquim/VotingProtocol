920f0fa123a0ac75a42bd068e7b6ae9e0da38598
Víctor Hugo <vhms@cin.ufpe.br>
2021-04-07 17:26:39 -0300

    Add states
    Add states in VotingClient.py, almost complete

--- src/VotingClient.py
+++ src/VotingClient.py
@@ -18,6 +18,7 @@ def main():
     msg = input()
     state = 'Client Data'
     Token = None
+    title = None
     while msg != '\x18': #trocar para while True e dar break no fim da conexão
         #aqui vai receber a msg do server
         #my_packet = tcp.recv(1024)
@@ -40,7 +41,7 @@ def main():
                 Token = my_packet.Token
             
         if state == 'Client Request':
-            comand = input('Criar sessão: 0\nConsultar sessões abertas: 1\nConsultar sessões fechadas: 1\n')
+            comand = input('Criar sessão: 0\nConsultar sessões abertas: 1\nConsultar sessões fechadas: 2\nVer detalhes de uma sessão: 3\nEncerrar conexão: 4')
             if(comand == 0):
                 my_packet = ClientRequestCreate()
                 my_packet.Token = Token
@@ -51,9 +52,9 @@ def main():
                 my_packet.Timeout = input('Até quando deve durar a sessão?')
                 my_packet.QtdOptions = int (input('Quantidade de opções'))
                 my_packet.Options = []
-                for i in range(1,my_packet.QtdOptions):
+                for i in range(my_packet.QtdOptions):
                     my_packet.Options.append(input('nome da opção ',i))
-
+                state = 'Request Response'
 
             if(comand == 1):
                 my_packet = ClientRequest()
@@ -61,6 +62,7 @@ def main():
                 my_packet.FlagCreateSession = False
                 my_packet.FlagAvailableSession = True
                 my_packet.FlagCompletedSession = False
+                state = 'Request Response'
 
             if(comand == 2):
                 my_packet = ClientRequest()
@@ -68,6 +70,70 @@ def main():
                 my_packet.FlagCreateSession = False
                 my_packet.FlagAvailableSession = False
                 my_packet.FlagCompletedSession = True
+                state = 'Request Response'
+
+            if(comand == 3):
+                title = input('Titulo da sessão: ')
+                my_packet = SessionDetails()
+                my_packet.Token = Token
+                my_packet.Title = title
+                state = 'Session Description'
+
+            if(comand == 4):
+                #exit
+                print('Até um outro dia!!')
+                break
+
+        if state == 'Request Response':
+            #recive msg
+            my_packet = SessionDescription()
+            print(my_packet.Title)
+            #verificar se nao deu erro, se der muda o estado
+            
+            if(my_packet.FlagFinished):
+                best = 0.0
+                winer = None
+                for i in my_packet.Options:
+                    g = i.split('=')
+                    s = ''
+                    for c in g[1]:
+                        if c != ' ' || c != '%':
+                            s += c
+                    s = float(s)
+                    winer = i if s > best else winer
+                    best = s if s > best else best
+                    print(i)
+                    wi = winer.split(' = ')
+                print('O vencedor foi: ',wi[0])
+                state = 'Client Request'
+            else:
+                for i in my_packet.Options:
+                    print(i)
+                print('Votação em andamento')
+                s = input('Deseja votar? Y/N')
+                s = s.lower()
+                if(s == 'y'):
+                    state = 'Vote'
+                else:
+                    state = 'Client Request'
+
+            if state == 'Vote':
+                title = input('Digite a sessão.')
+                option = input('Digite a opção')
+                my_packet = Vote()
+                my_packet.Token = Token
+                my_packet.Title = title
+                my_packet.Options = option
+                #envia
+                #tratar a resposta do voto
+                s = input('Deseja votar novamente? Y/N')
+                s = s.lower()
+                if(s == 'y'):
+                    state = 'Vote'
+                else:
+                    state = 'Client Request'
+
+                
 
         msg = input()
     tcp.close()
