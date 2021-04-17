2fd9133043a1ead7c7d3b6f7514aca160efa6a90
Riei Joaquim <rjmr@cin.ufpe.br>
2021-04-16 21:40:05 -0300

    minor fixes

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ from setuptools import setup, find_packages
 
 setup(name='VotingProtocol',
     version='1.0',
-    install_requires=['pycryptodome>=3.10.1','cryptography>=3.1.1'],
+    install_requires=['pycryptodome>=3.10.1','cryptography>=3.1.1', 'dataclasses>=0.6'],
     packages=['VotingProtocol']
 )
